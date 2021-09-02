14792ab17c436f46ac99a382d2d19f06b8f4fabe
Ryota Sago <sagoryota@gmail.com>
2021-09-02 23:57:53 +0900

    11,12,13章

--- src/main/scala/cpu/Core.scala
+++ src/main/scala/cpu/Core.scala
@@ -11,43 +11,87 @@ class Core extends Module {
         val dmem = Flipped(new DmemPortIo())
         val exit = Output(Bool())
     })
-
     val regfile = Mem(32, UInt(WORD_LEN.W))
 
+    // IF(Instruction Fetch) Stage
     val pc_reg = RegInit(START_ADDR)
     pc_reg := pc_reg + 4.U(WORD_LEN.W)
 
     io.imem.addr := pc_reg
     val inst = io.imem.inst
 
+    // ID(Instruction Decode) Stage
+    //
+    // |31                 25|24     20|19 15|14    12|11       8|        7|6      0|
+    // |        func7        |   rs2   | rs1 | func3  |         rd         | opcode | R形式
+    // |           imm[11:0]           | rs1 | func3  |         rd         | opcode | I形式
+    // |     imm[11:5]       |   rs2   | rs1 | func3  |      imm[4:0]      | opcode | S形式
+    // | imm[12] | imm[10:5] |   rs2   | rs1 | func3  | imm[4:1] | imm[11] | opcode | B形式
+    // |                imm[31:12]                    |         rd         | opcode | U形式
+    // | imm[20] | imm[10:1] | imm[11] |  imm[19:12]  |         rd         | opcode | J形式
     val rs1_addr = inst(19,15)
     val rs2_addr = inst(24,20)
     val wb_addr = inst(11,7)
     val rs1_data = Mux((rs1_addr =/= 0.U(WORD_LEN.U)), regfile(rs1_addr), 0.U(WORD_LEN.W))
     val rs2_data = Mux((rs2_addr =/= 0.U(WORD_LEN.U)), regfile(rs2_addr), 0.U(WORD_LEN.W))
 
-    val imm_i = inst(31, 20)
+    val imm_i = inst(31, 20) // I形式のimm[11:0]
     val imm_i_sext = Cat(Fill(20, imm_i(11)), imm_i)
 
-    val imm_s = Cat(inst(31, 25), inst(11, 7))
+    val imm_s = Cat(inst(31, 25), inst(11, 7)) // S形式のimm[11:5]
     val imm_s_sext =Cat(Fill(20, imm_s(11)), imm_s)
-    val alu_out = MuxCase(0.U(WORD_LEN.W), Seq(
-        (inst === LW)   -> (rs1_data + imm_i_sext),
-        (inst === SW)   -> (rs1_data + imm_s_sext),
-        (inst === ADD)  -> (rs1_data + rs2_data),
-        (inst === ADDI) -> (rs1_data + imm_i_sext),
-        (inst === SUB)  -> (rs1_data - rs2_data)
+
+    val csignals = ListLookup(inst,
+        List(ALU_X, OP1_RS1, OP2_RS2, MEN_X, REN_X, WB_X),
+        Array(
+            LW      ->  List(ALU_ADD, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_MEM),
+            SW      ->  List(ALU_ADD, OP1_RS1, OP2_IMS, MEN_S, REN_X, WB_X),
+            ADD     ->  List(ALU_ADD, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU),
+            ADDI    ->  List(ALU_ADD, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU),
+            SUB     ->  List(ALU_SUB, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU),
+            AND     ->  List(ALU_AND, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU),
+            OR      ->  List(ALU_OR,  OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU),
+            XOR     ->  List(ALU_XOR, OP1_RS1, OP2_RS2, MEN_X, REN_S, WB_ALU),
+            ANDI    ->  List(ALU_AND, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU),
+            ORI     ->  List(ALU_OR,  OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU),
+            XORI    ->  List(ALU_XOR, OP1_RS1, OP2_IMI, MEN_X, REN_S, WB_ALU),
+        )
+    )
+    val exe_fun :: op1_sel :: op2_sel :: mem_wen :: rf_wen :: wb_sel :: Nil = csignals
+    val op1_data = MuxCase(0.U(WORD_LEN.W), Seq(
+        (op1_sel === OP1_RS1) -> rs1_data
+    ))
+    val op2_data = MuxCase(0.U(WORD_LEN.W), Seq(
+        (op2_sel === OP2_RS2) -> rs2_data,
+        (op2_sel === OP2_IMI) -> imm_i_sext,
+        (op2_sel === OP2_IMS) -> imm_s_sext
     ))
 
+    // EX(Execute) Stage
+    val alu_out = MuxCase(0.U(WORD_LEN.W), Seq(
+        (exe_fun === ALU_ADD)   -> (op1_data + op2_data),
+        (exe_fun === ALU_SUB)   -> (op1_data - op2_data),
+        (exe_fun === ALU_AND)   -> (op1_data & op2_data),
+        (exe_fun === ALU_OR)    -> (op1_data | op2_data),
+        (exe_fun === ALU_XOR)   -> (op1_data ^ op2_data),
+    ))
+
+    // Mem access Stage
     io.dmem.addr := alu_out
-    io.dmem.wen := (inst === SW)
+    io.dmem.wen := mem_wen
     io.dmem.wdata := rs2_data
-    val wb_data = io.dmem.rdata
-    when(inst === LW) {
+
+    // WB(Writeback) Stage
+    val wb_data = MuxCase(alu_out, Seq(
+        (wb_sel === WB_MEM) -> io.dmem.rdata
+    ))
+
+    when(rf_wen === REN_S) {
         regfile(wb_addr) := wb_data
     }
     io.exit := (inst === 0x00602823.U(WORD_LEN.W))
 
+    // debug
     printf(p"pc_reg     : 0x${Hexadecimal(pc_reg)}\n")
     printf(p"inst       : 0x${Hexadecimal(inst)}\n")
     printf(p"rs1_addr   : $rs1_addr\n")
